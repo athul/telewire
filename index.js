28bc3fea2596c516c2cffe02216d7c7b333832b4
athul <athul8720@gmail.com>
2020-03-11 17:47:51 +0530

    Formatting

--- index.js
+++ index.js
@@ -26,66 +26,97 @@ const evresp = (gevent) => {
         case "issues":
             return `
             ❗️❗️❗️❗️❗️❗️
-            Issue ${prstate}
-            Issue Title and Number  : ${ititle} | #${inum}
-            Commented or Created By : \`${iactor}\`
-            Issue Body : *${ibody}*
-            [Link to Issue]("https://github.com/${repo}/issues/${inum}")
-            [Link to Repo ]("https://github.com/${repo}/")
-            [Build log here]("https://github.com/${repo}/commit/${sha}/checks")
+        
+        Issue ${prstate}
+        
+        Issue Title and Number  : ${ititle} | #${inum}
+        
+        Commented or Created By : \`${iactor}\`
+        
+        Issue Body : *${ibody}*
+        
+        [Link to Issue]("https://github.com/${repo}/issues/${inum}")
+        [Link to Repo ]("https://github.com/${repo}/")
+        [Build log here]("https://github.com/${repo}/commit/${sha}/checks")
                 `
         case "issue_comment":
             return `
             🗣🗣🗣🗣🗣🗣
-            Issue ${prstate}
-            Issue Title and Number  : ${ititle} | #${inum}
-            Commented or Created By : \`${iactor}\`
-            Issue Body : *${ibody}*
-            Issue Comment: \`${process.env.INPUT_IU_COM}\`
-            [Link to Issue]("https://github.com/${repo}/issues/${inum}")
-            [Link to Repo ]("https://github.com/${repo}/")
-            [Build log here]("https://github.com/${repo}/commit/${sha}/checks")
+        Issue ${prstate}
+
+        Issue Title and Number  : ${ititle} | #${inum}
+
+        Commented or Created By : \`${iactor}\`
+
+        Issue Body : *${ibody}*
+
+        Issue Comment: \`${process.env.INPUT_IU_COM}\`
+
+        [Link to Issue]("https://github.com/${repo}/issues/${inum}")
+        [Link to Repo ]("https://github.com/${repo}/")
+        [Build log here]("https://github.com/${repo}/commit/${sha}/checks")
             `
         case "pull_request":
             return `
             🔃🔀🔃🔀🔃🔀
-            PR ${prstate} 
-            PR Number:      ${pnum}
-            PR Title:       ${ptitle}
-            PR Body:        *${pbody}*
-            PR By:          ${ghactor}
-            [Link to Issue]("https://github.com/${repo}/pull/${pnum}")
-            [Link to Repo ]("https://github.com/${repo}/")
-            [Build log here]("https://github.com/${repo}/commit/${sha}/checks")
+        PR ${prstate} 
+        
+        PR Number:      ${pnum}
+        
+        PR Title:       ${ptitle}
+        
+        PR Body:        *${pbody}*
+        
+        PR By:          ${ghactor}
+        
+        [Link to Issue]("https://github.com/${repo}/pull/${pnum}")
+        [Link to Repo ]("https://github.com/${repo}/")
+        [Build log here]("https://github.com/${repo}/commit/${sha}/checks")
             `
         case "watch":
             return `
             ⭐️⭐️⭐️
-            By:            *${ghactor}* 
-            \`Repository:  ${repo}\` 
-            Star Count      ${process.env.INPUT_STARGAZERS}
-            Fork Count      ${process.env.INPUT_FORKERS}
-            [Link to Repo ]("https://github.com/${repo}/")
+        By:            *${ghactor}* 
+        
+        \`Repository:  ${repo}\` 
+        
+        Star Count      ${process.env.INPUT_STARGAZERS}
+        
+        Fork Count      ${process.env.INPUT_FORKERS}
+        
+        [Link to Repo ]("https://github.com/${repo}/")
             `
         case "schedule":
             return `
             ⏱⏰⏱⏰⏱⏰
-            ID: ${ghwrkflw}
-            Run *${ipstatus}!*
-            *Action was Run on Schedule*
-            \`Repository:  ${repo}\` 
-            [Link to Repo ]("https://github.com/${repo}/")
+        
+        ID: ${ghwrkflw}
+        
+        Run *${ipstatus}!*
+        
+        *Action was Run on Schedule*
+        
+        \`Repository:  ${repo}\` 
+        
+        [Link to Repo ]("https://github.com/${repo}/")
             `
         default:
             return `
             ⬆️⇅⬆️⇅
-            ID: ${ghwrkflw}
-            Action was a *${ipstatus}!*
-            \`Repository:  ${repo}\` 
-            On:          *${ghevent}*
-            By:            *${ghactor}* 
-            Tag:        ${process.env.GITHUB_REF}
-            [Link to Repo ]("https://github.com/${repo}/")
+            
+        ID: ${ghwrkflw}
+        
+        Action was a *${ipstatus}!*
+        
+        \`Repository:  ${repo}\` 
+        
+        On:          *${ghevent}*
+        
+        By:            *${ghactor}* 
+        
+        Tag:        ${process.env.GITHUB_REF}
+        
+        [Link to Repo ]("https://github.com/${repo}/")
             `
     }
 }
